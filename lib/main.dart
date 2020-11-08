ae51b1ac295830ac35508e7f9efb9eca63211f06
Nicat <iamnicathaciyev13@gmail.com>
2020-11-08 15:52:24 +0400

    main class is updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app_with_hive/data/models/todo_data.dart';
 import 'package:todo_app_with_hive/presentation/pages/task_page.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todo_app_with_hive/presentation/widgets/error_dialog.dart';
+import 'package:todo_app_with_hive/presentation/widgets/progress_indicator.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(TodoDataAdapter());
+
   runApp(MyApp());
 }
 
@@ -9,7 +17,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TaskPage(),
+      home: FutureBuilder(
+        future: Hive.openBox('todo_tasks'),
+        builder: (BuildContext context, AsyncSnapshot<Box<dynamic>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return ErrorDialog(snapshot: snapshot);
+            } else {
+              return TaskPage();
+            }
+          } else {
+            return CustomProgressIndicator();
+          }
+        },
+      ),
     );
   }
 }
