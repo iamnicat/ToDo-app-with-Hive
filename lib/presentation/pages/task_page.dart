3deb749393ad84a5c45199a0591053ebf3d469be
Nicat <iamnicathaciyev13@gmail.com>
2020-11-08 11:01:36 +0400

    Task Page is updated

--- lib/presentation/pages/task_page.dart
+++ lib/presentation/pages/task_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:todo_app_with_hive/presentation/widgets/task_list.dart';
 import 'task_add_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class TaskPage extends StatelessWidget {
   @override
@@ -24,54 +27,60 @@ class TaskPage extends StatelessWidget {
           );
         },
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: EdgeInsets.only(
-                top: 60.0, right: 30.0, bottom: 30.0, left: 30.0),
-            child: Column(
-              children: [
-                CircleAvatar(
-                  backgroundColor: Colors.white,
-                  radius: 30.0,
-                  child: Icon(
-                    Icons.list,
-                    size: 30.0,
-                    color: Color(0xFFD81B60),
-                  ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Text(
-                  'EY',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 50.0,
-                      fontWeight: FontWeight.w700,
-                      letterSpacing: 4.0),
-                ),
-                Text(
-                  '0 tasks',
-                  style: TextStyle(color: Colors.white, fontSize: 15.0),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20.0),
-                  topRight: Radius.circular(20.0),
+      body: ValueListenableBuilder(
+        valueListenable: Hive.box('todo_tasks').listenable(),
+        builder: (BuildContext context, box, Widget child) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.only(
+                    top: 60.0, right: 30.0, bottom: 30.0, left: 30.0),
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 30.0,
+                      child: Icon(
+                        Icons.list,
+                        size: 30.0,
+                        color: Color(0xFFD81B60),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      'EY',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.w700,
+                          letterSpacing: 4.0),
+                    ),
+                    Text(
+                      '0 tasks',
+                      style: TextStyle(color: Colors.white, fontSize: 15.0),
+                    )
+                  ],
                 ),
               ),
-            ),
-          ),
-        ],
+              Expanded(
+                child: Container(
+                  child: TaskList(),
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
