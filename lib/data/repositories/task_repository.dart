358c4f94defaa44c1344214ad7078f0fa497f2d2
Nicat <iamnicathaciyev13@gmail.com>
2020-11-08 15:50:42 +0400

    some simple security changes

--- lib/data/repositories/task_repository.dart
+++ lib/data/repositories/task_repository.dart
@@ -2,34 +2,34 @@ import 'package:todo_app_with_hive/data/models/todo_data.dart';
 import 'package:hive/hive.dart';
 
 class TaskRepository {
-  TodoData todoData;
+  final TodoData todoData;
 
   TaskRepository({this.todoData});
 
-  Box taskBox = Hive.box('todo_tasks');
+  Box _taskBox = Hive.box('todo_tasks');
 
   var _filteredData = Hive.box('todo_tasks').values.where((element) {
     return element.isDone == false;
   });
 
   void addTask(TodoData todoData) {
-    taskBox.add(todoData);
+    _taskBox.add(todoData);
   }
 
   TodoData getAllTask(int index) {
-    return taskBox.getAt(index);
+    return _taskBox.getAt(index);
   }
 
   void updateTask(int index, TodoData todoData) {
-    taskBox.putAt(
+    _taskBox.putAt(
         index, TodoData(task: todoData.task, isDone: !todoData.isDone));
   }
 
   void deleteTask(int index) {
-    taskBox.deleteAt(index);
+    _taskBox.deleteAt(index);
   }
 
-  int getDataLength() => taskBox.length;
+  int getDataLength() => _taskBox.length;
 
   int getUnfinishedDataLength() => _filteredData.length;
 }
