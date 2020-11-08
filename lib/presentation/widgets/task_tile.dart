b003003101065995863dd3a16be136162abf9109
Nicat <iamnicathaciyev13@gmail.com>
2020-11-08 11:18:13 +0400

    onLongPress function is created to delete task

--- lib/presentation/widgets/task_tile.dart
+++ lib/presentation/widgets/task_tile.dart
@@ -4,21 +4,29 @@ class TaskTile extends StatelessWidget {
   final bool isChecked;
   final String taskText;
   final Function checkboxCallback;
+  final Function onLongPressCallback;
 
-
-  TaskTile({@required this.taskText, @required this.isChecked,@required this.checkboxCallback});
+  TaskTile(
+      {@required this.taskText,
+      @required this.isChecked,
+      @required this.checkboxCallback,
+      @required this.onLongPressCallback});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        taskText,
-        style: TextStyle(
-            color: Colors.black,
-            decoration: isChecked ? TextDecoration.lineThrough : null),
-      ),
-      trailing: Checkbox(
-        value: isChecked, onChanged: checkboxCallback,
+    return GestureDetector(
+      onLongPress: onLongPressCallback,
+      child: ListTile(
+        title: Text(
+          taskText,
+          style: TextStyle(
+              color: Colors.black,
+              decoration: isChecked ? TextDecoration.lineThrough : null),
+        ),
+        trailing: Checkbox(
+          value: isChecked,
+          onChanged: checkboxCallback,
+        ),
       ),
     );
   }
